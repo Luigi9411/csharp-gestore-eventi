b94ec098f80fed96518dbf020d937abafec9a845
Luigi9411 <luigi.sciavicco@gmail.com>
2023-03-28 21:23:37 +0200

    milstone1

---
+++ GestoreEventi/Event.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi
+{
+    public class Event
+    {
+        public string Title {get;set;}
+
+        public string Date { get;set;}
+
+        public int MaximumCapacityOftheEvent { get; private set; }
+
+        public int NumberOfSeatsBooked { get; private set;}
+    }
+}
