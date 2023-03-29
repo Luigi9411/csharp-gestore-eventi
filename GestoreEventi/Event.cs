501315e15908939b92a2dc2713e53e4feb2b49fd
Luigi9411 <luigi.sciavicco@gmail.com>
2023-03-29 23:16:12 +0200

    milestone2

--- GestoreEventi/Event.cs
+++ GestoreEventi/Event.cs
@@ -2,18 +2,115 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace GestoreEventi
 {
     public class Event
     {
-        public string Title {get;set;}
+        private string title;
+        private DateTime date;
+        private int maximumCapacity;
+        private int reservedSeats;
+        public Event(string title, DateTime date, int maximumCapacity)
+        {
+            Title = title;
+            Date = date;
+            MaximumCapacity = maximumCapacity;
+        }
 
-        public string Date { get;set;}
+        public string Title
+        {
+            get { return title; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Title cannot be left empty.");
+                }
 
-        public int MaximumCapacityOftheEvent { get; private set; }
+                title = value;
+            }
+        }
 
-        public int NumberOfSeatsBooked { get; private set;}
+        public DateTime Date
+        {
+            get { return date; }
+            private set
+            {
+                if (value < DateTime.Now)
+                {
+                    throw new ArgumentException("La data deve essere successiva alla giornaliera.");
+                }
+
+                date = value;
+            }
+        }
+
+        public int MaximumCapacity
+        {
+            get { return maximumCapacity; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("La capienza deve essere un numero intero positivo.");
+                }
+
+                maximumCapacity = value;
+            }
+        }
+
+        public int ReservedSeats { get; private set;}
+
+        public int PlacesAvailable
+        {
+            get { return MaximumCapacity - ReservedSeats; }
+            private set{ }
+        }
+
+        public void BookSeats(int seats)
+        {
+            EnsureDateIsNotPast();
+
+            if ( maximumCapacity is 0)
+            {
+                throw new InvalidOperationException("Questo evento non prevede posti, non è possibile prenotare.");
+            }
+
+            if (PlacesAvailable < seats)
+            {
+                throw new InvalidOperationException("Posti disponibili insufficienti.");
+            }
+
+            ReservedSeats += seats;
+        }
+
+        public void CancelPlaces(int seats)
+        {
+            EnsureDateIsNotPast();
+
+            if (ReservedSeats < seats)
+            {
+                throw new InvalidOperationException("Prenotazioni da annullare.");
+            }
+
+            ReservedSeats -= seats;
+        }
+
+
+        public override string ToString()
+        {
+            return $"{Date.ToString("dd/MM/yyyy")} - {Title}";
+        }
+
+        private  void EnsureDateIsNotPast()
+        {
+            if (Date < DateTime.Now)
+            {
+                throw new InvalidOperationException("Impossibile prenotare, l'evento è passato.");
+            }
+        }
     }
 }
--- GestoreEventi/Program.cs
+++ GestoreEventi/Program.cs
@@ -1,39 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Runtime.ConstrainedExecution;
-Console.WriteLine("Hello, World!");
+﻿using GestoreEventi;
 
-//Per prima cosa è necessario creare una classe Evento che abbia i seguenti attributi:
-//●	titolo
-//●	data
-//●	capienza massima dell’evento
-//●	numero di posti prenotati
+Console.WriteLine("Inserisci un nuovo evento:");
 
-//Aggiungere metodi getter e setter in modo che:
-//●	titolo sia in lettura e in scrittura
-//●	data sia in lettura e scrittura
-//●	numero di posti per la capienza massima sia solo in lettura
-//●	numero di posti prenotati sia solo in lettura
-//ai setters inserire gli opportuni controlli in modo che la data non sia già passata,
-//che il titolo non sia vuoto e che la capienza massima di posti sia un numero positivo. 
-//In caso contrario sollevare opportune eccezioni.
+Console.WriteLine("Titolo evento:");
+string title = Console.ReadLine()!;
 
-//Dichiarare un costruttore che prenda come parametri il titolo, la data e i posti a disposizione
-//e inizializza gli opportuni attributi facendo uso dei metodi e controlli già fatti.
-//Per l’attributo posti prenotati invece si occupa di inizializzarlo lui a 0.
+Console.WriteLine("Data evento:");
+DateTime date = Convert.ToDateTime(Console.ReadLine());
 
-//Vanno inoltre implementati dei metodi public che svolgono le seguenti funzioni:
+Console.WriteLine("Capienza massima:");
+int maximumCapacity = Convert.ToInt32(Console.ReadLine());
 
-//1.PrenotaPosti: aggiunge i posti passati come parametro ai posti prenotati.
-//    Se l’evento è già passato o non ha posti o non ha più posti disponibili deve sollevare un’eccezione.
-//2.	DisdiciPosti: riduce del i posti prenotati del numero di posti indicati come parametro. 
-//    Se l’evento è già passato o non ci sono i posti da disdire sufficienti, deve sollevare un’eccezione.
-//3.	l’override del metodo ToString() in modo che venga restituita una stringa contenente: data formattata – titolo
-//Per formattare la data correttamente usate nomeVariabile.ToString("dd/MM/yyyy"); 
-//applicata alla vostra variabile DateTime.
+var @event = new Event(title, date, maximumCapacity);
 
-//Le eccezioni possono essere generiche (Exception) o usate quelle già messe a disposizione da C#,
-//    ma aggiungete un eventuale messaggio chiaro per comunicare che cosa è successo.
+Console.WriteLine("Vuoi fare prenotazioni? (0 = no, altrimenti specificare numero)");
+int reservations = Convert.ToInt32(Console.ReadLine());
 
-//Vi ricordo che man mano che andrete avanti con le altre milestones,
-//potrete aggiungere più avanti altri eventuali metodi (public e private)
-//    che vi aiutino a svolgere le funzioni richieste se ritenete necessari! 
+@event.BookSeats(reservations);
+
+Console.WriteLine($"Numero dei posti prenotati = {@event.ReservedSeats}.");
+Console.WriteLine($"Numero dei posti disponibili = {@event.PlacesAvailable}.");
+
+Console.Write("Vuoi disdire dei posti ? (si/no): ");
+string input = Console.ReadLine();
+
+while (input.ToLower() == "si")
+{
+    Console.Write("Quanti posti vuoi disdire dei posti? ");
+    int seats = int.Parse(Console.ReadLine());
+
+    try
+    {
+        @event.CancelPlaces(seats);
+        var postiDisponibili = @event.MaximumCapacity - @event.ReservedSeats;
+        Console.WriteLine($"Numero posti disdetti = {seats}");
+        Console.WriteLine($"Numero posti disponibili = {@event.PlacesAvailable}");
+    }
+    catch (ArgumentException exception)
+    {
+        Console.WriteLine(exception.Message);
+    }
+
+    Console.Write("Quanti posti vuoi disdire dei posti? (si/no): ");
+    input = Console.ReadLine();
+}
+Console.Write("Ok va bene"); 
+Console.ReadLine();
+
